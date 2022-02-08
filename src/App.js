5d00e81da542c19a4f8e1285433ae706b4af3767
Michi Berger <mberger83@yahoo.de>
2022-02-08 13:21:52 +0100

    Add App

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import './App.css';
 import Room from './Room.js';
 import Header from './Header.js';
 
-// Alternativ : export default App() {}
-function App() {
-  //const mit einem Array erzeugen
-  //über Array mit map iterieren
+export default function App() {
   const rooms = [
     {
       text: 'Küche',
@@ -28,21 +25,19 @@ function App() {
   ];
 
   return (
-    <div className="App">
+    <main className="App">
       <Header>Happy Cleaning</Header>
-      <main>
-        {rooms.map(({ text, description, isDescriptionVisible, isClean }) => {
-          <Room
-            key={text}
-            text={text}
-            description={description}
-            isDescriptionVisible={isDescriptionVisible}
-            isClean={isClean}
-          />;
-        })}
-      </main>
-    </div>
+      {rooms.map(({ text, description, isDescriptionVisible, isClean }) => (
+        <Room
+          key={text}
+          text={text}
+          description={description}
+          isDescriptionVisible={isDescriptionVisible}
+          isClean={isClean}
+        />
+      ))}
+    </main>
   );
 }
-// wenn export default App() {}
-export default App;
+// wenn function App(){} --> export default App;
+
