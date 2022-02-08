175bf60af4be809f5f5571bcceb6aa2afd7ef848
Michi Berger <mberger83@yahoo.de>
2022-02-08 10:51:00 +0100

    Create basic Room component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Room from './Room.js';
 
 // Alternativ : export default App() {}
 function App() {
-  return 'Hello world';
+  return (
+    <main className="App">
+      <Room text="Küche" isClean/>
+      <Room text="Wohnzimmer" isClean={false}/>
+      <Room text="Bad" />
+    </main>
+  );
 }
-
+// wenn export default App() {}
 export default App;
---
+++ src/Room.js
@@ -0,0 +1,5 @@
+import './Room.css'
+
+export default function Room({text, isClean}) {
+  return (<section className="Room">{ text } {isClean ? 'grün' : 'rot'}</section>);
+}
